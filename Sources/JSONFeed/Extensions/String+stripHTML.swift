5e0094f80b0689bb766a3ceef961576febccfc7c
Bryce Campbell <tonyhawk2100@gmail.com>
2019-09-01 08:00:21 -0700

    fix problem with returning plain text.

--- Sources/JSONFeed/Extensions/String+stripHTML.swift
+++ Sources/JSONFeed/Extensions/String+stripHTML.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 extension String {
-    public func stripHTML() {
+    public func stripHTML() -> String {
         return self.replacingOccurrences(of: "<[^>]+>", with: "", options: NSString.CompareOptions.regularExpression, range: nil)
     }
 }
